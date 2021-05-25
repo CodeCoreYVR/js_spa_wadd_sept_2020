6c4c2b8d5d832e4bfff5a5a1cf129dc1371aff3f
helgakern <helgakern@gmail.com>
2021-05-25 12:03:17 -0700

    Added create question method and session to mock login

--- spa/js/main.js
+++ spa/js/main.js
@@ -9,7 +9,39 @@ const Question = {
             return res.json();
         })
     },
+
+    create(params){
+        return fetch(`${BASE_URL}/questions`, {
+           method: 'POST',
+           credentials: 'include', 
+           headers: {
+               'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(params) 
+        }).then((res) => res.json());
+    },
+
 }
+
+const Session = {
+    create(params){
+        return fetch(`${BASE_URL}/session`, {
+            method: 'POST',
+            credentials: 'include',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        })
+    }
+}
+
+Session.create({
+    email: 'js@winterfell.gov',
+    password: 'supersecret'
+}).then(console.log);
+
+
 
 Question.index()
     .then(questions => {
@@ -22,5 +54,20 @@ Question.index()
             `
         }).join('');
     })
+
+const newQuestionForm = document.querySelector('#new-question-form');    
+newQuestionForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = event.currentTarget
+    const formData = new FormData(form);
+    const newQuestionParams = {
+        title: formData.get('title'),
+        body: formData.get('body')
+    }
+    Question.create(newQuestionParams)
+    .then(data => {
+        console.log(data);
+    })
+})
 
 
