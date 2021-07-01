03ae57839c64f366707f15db9d132a71697bbfb3
helgakern <helgakern@gmail.com>
2021-06-30 17:48:00 -0700

    fix current user session

--- awesome-answers/db/schema.rb
+++ awesome-answers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_163808) do
+ActiveRecord::Schema.define(version: 2021_06_24_211133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2021_04_14_163808) do
     t.bigint "user_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "gifts", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "receiver_id"
+    t.float "amount"
+    t.string "txn_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "job_posts", force: :cascade do |t|
